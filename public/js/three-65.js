16a08521da9988c72e4dc4d2d1753081224f3387
Aaron Goshine <greenlig@gmail.com>
2018-06-12 14:13:49 +0100

    feat(Remover): delete items that throws error

--- public/js/three-65.js
+++ public/js/three-65.js
@@ -1,28 +1,28 @@
-/* global YT, $*/
+/* global YT, $ alert */
 
 /*
  * @var {boolean} PLAY_TODAY - whither to just repeat today's track automatically
  */
-var PLAY_TODAY = true;
+var PLAY_TODAY = true
 
 /**
  * @var {string} DEFAULT_PLAY_ID - default id if everything goes wrong the player will resort
  */
-var DEFAULT_PLAY_ID = 'Ua2loiGHZ38';
+var DEFAULT_PLAY_ID = 'Ua2loiGHZ38'
 
 /**
  * @var {object} player - reference to the current instant of the Youtube iframe player
  */
-var player;
+var player
 
 /**
  * @var {number} playlistIndex - The current index of the 365 play list
  */
-var playlistIndex = 0;
+var playlistIndex = 0
 
-var rainVolume = 0.2;
+var rainVolume = 0.2
 
-var repeat = false;
+var repeat = false
 /**
  * @var quickStore - is an interface to the current playlist
  */
@@ -34,27 +34,27 @@ var quickStore = {
    * @param {array} data - Array containing objects @like `{id : ...., name: ......}`
    */
   updateStore: function (data) {
-    this.data = data;
+    this.data = data
     for (var i = 0; i < data.length; i++) {
-      var currentItem = data[i];
-      this.dataHash[i] = currentItem;
-      this.dataHash[currentItem.id] = currentItem;
-      this.dataHash[currentItem.id + '_idx'] = i;
+      var currentItem = data[i]
+      this.dataHash[i] = currentItem
+      this.dataHash[currentItem.id] = currentItem
+      this.dataHash[currentItem.id + '_idx'] = i
     }
   },
   getItemById: function (id) {
-    return this.dataHash[id];
+    return this.dataHash[id]
   },
   getItemByIndex: function (index) {
-    return this.dataHash[index];
+    return this.dataHash[index]
   },
   getMaxIndex: function () {
-    return this.data.length;
+    return this.data.length
   },
   getIndexById: function (videoId) {
-    return this.dataHash[videoId + '_idx'];
+    return this.dataHash[videoId + '_idx']
   }
-};
+}
 
 /**
  * @function calculateDayIndex - calculate the number of the current day of the current year
@@ -62,12 +62,12 @@ var quickStore = {
  * @return {number}
  */
 var calculateDayIndex = function () {
-  var currentDate = new Date();
-  var startOfYear = new Date(currentDate.getFullYear(), 0, 1);
-  var timeOffSet = currentDate.getTime() - startOfYear.getTime();
-  var dayOfYear = Math.ceil(timeOffSet / 86400000);
-  return dayOfYear > 0 ? dayOfYear - 1 : dayOfYear;
-};
+  var currentDate = new Date()
+  var startOfYear = new Date(currentDate.getFullYear(), 0, 1)
+  var timeOffSet = currentDate.getTime() - startOfYear.getTime()
+  var dayOfYear = Math.ceil(timeOffSet / 86400000)
+  return dayOfYear > 0 ? dayOfYear - 1 : dayOfYear
+}
 
 /**
  * @function getNextPlayId - returns the next Id in the playlist
@@ -75,75 +75,76 @@ var calculateDayIndex = function () {
  */
 var getNextPlayId = function () {
   if (playlistIndex >= quickStore.getMaxIndex() || playlistIndex < 0) {
-    playlistIndex = 0;
+    playlistIndex = 0
   } else {
     if (!repeat) {
-      playlistIndex += 1;
+      playlistIndex += 1
     }
   }
 
-  var item = quickStore.getItemByIndex(playlistIndex);
+  var item = quickStore.getItemByIndex(playlistIndex)
   if (item) {
-    return item.id;
+    return item.id
   } else {
-    return getTodaysPlayId();
+    return getTodaysPlayId()
   }
-};
+}
 
 /**
  * @function getTodaysPlayId - returns an id base on today's index
  * @return {string} - Youtube media id
  */
 var getTodaysPlayId = function () {
-  var item = quickStore.getItemByIndex(calculateDayIndex());
+  var item = quickStore.getItemByIndex(calculateDayIndex())
   if (item) {
-    return item.id;
+    return item.id
   }
-};
+}
 
 /**
  * @function onEnded - is an event handler the is fired when each track has ended
  */
 var onEnded = function (event) {
-  playById(getNextPlayId());
-};
+  playById(getNextPlayId())
+}
 
 /**
  * @function onPlaying - is an event handler the is fired when each track has resumed
  */
 var onPlaying = function (event) {
-  console.log('on playing');
-};
+  console.log('on playing')
+}
 
 /**
  * @function onPaused - is an event handler the is fired when each track is paused
  */
 
 var onPaused = function (event) {
-  console.log('on paused');
-};
+  console.log('on paused')
+}
 
 /**
  * @function stopVideo - is a wrapper to the current player.stopVideo()
  */
 var stopVideo = function () {
-  player.stopVideo();
-};
+  player.stopVideo()
+}
 
 /**
  * @function onPlayerReady - is an event handler that is fired when the
  * player is fully loading and ready to play
  */
 var onPlayerReady = function (event) {
-  event.target.playVideo();
-};
+  event.target.playVideo()
+}
 
 /**
  * @function onError - is an event handler that is fired when there is an exception
  */
 var onError = function (event) {
-  playById(getNextPlayId());
-};
+  var itemAtIndex = quickStore.getItemByIndex(playlistIndex)
+  deleteById(itemAtIndex)
+}
 
 /**
  * @function playDefaultQueId
@@ -151,12 +152,12 @@ var onError = function (event) {
  */
 var playDefaultQueId = function (erro) {
   if (erro) {
-    playById(DEFAULT_PLAY_ID);
+    playById(DEFAULT_PLAY_ID)
   }
-  var id = getTodaysPlayId();
-  playlistIndex = quickStore.getIndexById(id);
-  return id;
-};
+  var id = getTodaysPlayId()
+  playlistIndex = quickStore.getIndexById(id)
+  return id
+}
 
 /**
  * @function onPlayerStateChange - is an primary event delegate that is will trigger
@@ -166,15 +167,15 @@ var playDefaultQueId = function (erro) {
 var onPlayerStateChange = function (event) {
   switch (event.data) {
     case YT.PlayerState.ENDED:
-      onEnded(event);
-      break;
+      onEnded(event)
+      break
     case YT.PlayerState.PLAYING:
-      onPlaying(event);
-      break;
+      onPlaying(event)
+      break
     case YT.PlayerState.PAUSED:
-      onPaused(event);
+      onPaused(event)
   }
-};
+}
 
 /**
  * @function onYouTubeIframeAPIReady is the initialization method to configure the initial
@@ -192,8 +193,8 @@ var onYouTubeIframeAPIReady = function () {
       'onStateChange': onPlayerStateChange,
       'onError': onError
     }
-  });
-};
+  })
+}
 
 /**
  * @function playById  - this function plays and item via and id,
@@ -202,37 +203,37 @@ var onYouTubeIframeAPIReady = function () {
  *
  */
 var playById = function (videoId) {
-  player.loadVideoById(videoId);
-  playlistIndex = quickStore.getIndexById(videoId);
-  var itemAtIndex = quickStore.getItemByIndex(playlistIndex);
-  $('.now-playing').html((playlistIndex + 1) + ' ' + itemAtIndex.title);
-  $('.item-outline').removeClass('item-outline');
-  $('#' + videoId).parent().addClass('item-outline');
-};
+  player.loadVideoById(videoId)
+  playlistIndex = quickStore.getIndexById(videoId)
+  var itemAtIndex = quickStore.getItemByIndex(playlistIndex)
+  $('.now-playing').html((playlistIndex + 1) + ' ' + itemAtIndex.title)
+  $('.item-outline').removeClass('item-outline')
+  $('#' + videoId).parent().addClass('item-outline')
+}
 /**
  * @function playInputId
  * @desc - event handler
  */
 function playInputId () {
-  var videoIdOrUrl = $('#YTVID').val().trim();
+  var videoIdOrUrl = $('#YTVID').val().trim()
   // * 11 is the current length of Youtube videoID
   if (videoIdOrUrl.length === 11) {
-    playById(videoIdOrUrl);
+    playById(videoIdOrUrl)
   }
 
-  var comment = $('#COMM').val().trim();
+  var comment = $('#COMM').val().trim()
 
   $.ajax({
     type: 'POST',
     url: '/api/add',
     data: JSON.stringify({'mediaurl': videoIdOrUrl, 'comment': comment}),
     success: function (playlist) {
-      renderPlaylist(playlist);
+      renderPlaylist(playlist)
     },
     contentType: 'application/json',
     dataType: 'json'
-  });
-  $('.isertion-form-container').hide();
+  })
+  $('.isertion-form-container').hide()
 }
 
 /**
@@ -248,143 +249,142 @@ function deleteById (id) {
     url: '/api/delete',
     data: JSON.stringify({'id': id}),
     success: function (playlist) {
-      renderPlaylist(playlist);
+      renderPlaylist(playlist)
     },
     contentType: 'application/json',
     dataType: 'json'
-  });
+  })
 }
 
 function renderPlaylist (playlist) {
-  quickStore.updateStore(playlist);
-  var $availableList = $('#available-list');
-  $availableList.empty();
-  var numItems = quickStore.getMaxIndex();
-  var todayIndex = calculateDayIndex();
+  quickStore.updateStore(playlist)
+  var $availableList = $('#available-list')
+  $availableList.empty()
+  var numItems = quickStore.getMaxIndex()
+  var todayIndex = calculateDayIndex()
   for (var i = 0; i < numItems; i++) {
-    var itemAtIndex = quickStore.getItemByIndex(i);
-    var element = $('<div class="list-item"/>');
-    element.append($('<img src="' + itemAtIndex.thumbnail.url + '"/>'));
-    element.append($('<a id="' + itemAtIndex.id + '">&#9658; ' + (i + 1) + '</a>'));
-    element.append($('<p>' + itemAtIndex.title + '</a>'));
-    element.append($('<div class="delete tinybtn" data-id="' + itemAtIndex.id + '"> &#215;</a>'));
+    var itemAtIndex = quickStore.getItemByIndex(i)
+    var element = $('<div class="list-item"/>')
+    element.append($('<img src="' + itemAtIndex.thumbnail.url + '"/>'))
+    element.append($('<a id="' + itemAtIndex.id + '">&#9658; ' + (i + 1) + '</a>'))
+    element.append($('<p>' + itemAtIndex.title + '</a>'))
+    element.append($('<div class="delete tinybtn" data-id="' + itemAtIndex.id + '"> &#215;</a>'))
     if (itemAtIndex.comment) {
-      element.append($('<div class="info tinybtn" data-comment="' + itemAtIndex.comment + '" data-id="' + itemAtIndex.id + '"> &#9829;</a>'));
+      element.append($('<div class="info tinybtn" data-comment="' + itemAtIndex.comment + '" data-id="' + itemAtIndex.id + '"> &#9829;</a>'))
     }
 
     if (todayIndex === i) {
-      element.addClass('today');
+      element.addClass('today')
     }
 
-    $availableList.append(element);
+    $availableList.append(element)
   }
 
   $availableList.click(function (event) {
-    var videoId;
+    var videoId
     if ($(event.target).prop('tagName') === 'A') {
-      videoId = $(event.target).attr('id');
-      playById(videoId);
+      videoId = $(event.target).attr('id')
+      playById(videoId)
     }
 
     if ($(event.target).hasClass('delete')) {
-      videoId = $(event.target).data('id');
-      deleteById(videoId);
+      videoId = $(event.target).data('id')
+      deleteById(videoId)
     }
 
     if ($(event.target).hasClass('info')) {
-      var comment = $(event.target).data('comment');
-      alert(comment);
-      return null;
+      var comment = $(event.target).data('comment')
+      alert(comment)
+      return null
     }
-  });
+  })
 }
 
 $(document).ready(function () {
   // -- set default volume
-  var rain = document.getElementById('rain-control');
-  rain.volume = rainVolume;
+  var rain = document.getElementById('rain-control')
+  rain.volume = rainVolume
 
   $('#repeat').change(function (event) {
-    repeat = !repeat;
-  });
+    repeat = !repeat
+  })
 
   $('#YTVID').keydown(function (event) {
     if (event.which === 13) {
-      event.preventDefault();
-      playInputId();
+      event.preventDefault()
+      playInputId()
     }
-  });
+  })
 
   $('#COMM').keydown(function (event) {
-    var remianing_chars = 140 - $(event.target).val().length;
-    $('#char-count').html(remianing_chars);
-    if (event.which !== 8 && remianing_chars <= 0) {
-      event.preventDefault();
+    var remianingChars = 140 - $(event.target).val().length
+    $('#char-count').html(remianingChars)
+    if (event.which !== 8 && remianingChars <= 0) {
+      event.preventDefault()
     }
-  });
+  })
 
-  $('.isertion-form-container').hide();
+  $('.isertion-form-container').hide()
 
   $('#close-add-panel').click(function (event) {
-    $('.isertion-form-container').hide();
-  });
+    $('.isertion-form-container').hide()
+  })
 
   $('#add-track').click(function (event) {
-    $('.isertion-form-container').show();
-  });
+    $('.isertion-form-container').show()
+  })
 
   $('body').keydown(function (event) {
-    console.log(event.which);
+    console.log(event.which)
     switch (event.which) {
-        // Escape key for closing the modal
+      // Escape key for closing the modal
       case 27:
-        window.location.assign('/#');
-        break;
+        window.location.assign('/#')
+        break
         // letter 'c' changer track
       case 67:
         if (window.location.hash !== '#playlist') {
-          window.location.assign('/#playlist');
+          window.location.assign('/#playlist')
         }
-        break;
+        break
         // letter 'r' for rain
       case 82:
-        rainVolume = (rainVolume > 0 ? 0 : 0.2);
-        rain.volume = rainVolume;
-        break;
+        rainVolume = (rainVolume > 0 ? 0 : 0.2)
+        rain.volume = rainVolume
+        break
       case 37:
       case 38:
       case 75:
         // Arrow keys left and up
-        playlistIndex -= 2;
-        playById(getNextPlayId());
+        playlistIndex -= 2
+        playById(getNextPlayId())
         // play prev
-        break;
+        break
       case 39:
       case 40:
       case 74:
         // Arrow keys right and down
-        playById(getNextPlayId());
+        playById(getNextPlayId())
     }
 
     if (event.which === 13) {
-      event.preventDefault();
-      playInputId();
+      event.preventDefault()
+      playInputId()
     }
-  });
+  })
 
   $('#PLAY').click(function () {
-    playInputId();
-  });
+    playInputId()
+  })
 
   $.get('/api/playlist', function (playlist) {
-    renderPlaylist(playlist);
+    renderPlaylist(playlist)
     /**
      * Create script tag to source youtube iframe player
      */
-    var tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/iframe_api';
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  });
-});
-
+    var tag = document.createElement('script')
+    tag.src = 'https://www.youtube.com/iframe_api'
+    var firstScriptTag = document.getElementsByTagName('script')[0]
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+  })
+})
